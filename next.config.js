5070451c70951a132da2a5df010e39e8f1721c51
rayn-health <reezahbassier@gmail.com>
2026-08-24 00:24:28 +0200

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,2 @@
+/** @type {import('next').NextConfig} */
+module.exports={reactStrictMode:true,images:{remotePatterns:[{protocol:"https",hostname:"firebasestorage.googleapis.com"},{protocol:"https",hostname:"lh3.googleusercontent.com"}]}};
